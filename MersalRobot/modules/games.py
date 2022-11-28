b51f4b2bbf19b5a6f3a896e60531dd997f1a7210
GodseXD <118837216+GodseXD@users.noreply.github.com>
2022-11-28 12:31:02 +0530

    Update games.py

--- MersalRobot/modules/games.py
+++ MersalRobot/modules/games.py
@@ -83,6 +83,7 @@ async def _(event):
 
 
 __help__="""
+/subway surfers  https://poki.com/en/g/subway-surfers
 /dice or /dice 1 to 6 any value
 /ball or /ball 1 to 5 any value
 /dart or /dart 1 to 6 any value
